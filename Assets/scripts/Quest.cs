2142ee9ed3d412cba47f63c44b9b37a5d5526bcf
JohanneJayde <jmcclenahan@ewu.edu>
2023-07-28 14:47:25 -0700

    altered title screen and create new user page to fit with changes to app.

---
+++ Assets/scripts/Quest.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Quest
+{
+
+
+public string Code {get;}
+public string Description {get;}
+public string Directions {get;}
+public int Level {get;}
+public string Session {get;}
+public string ShortAnswer {get;}
+public string Theme {get;}
+public string TimeCompletion {get;}
+public string Title {get;}
+public string Type {get;}
+
+
+}
